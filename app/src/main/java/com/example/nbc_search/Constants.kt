cd9e809f7ec5b0d7c710cd26fd6195c9874cd95b
Juseonghyun <wntjdgus99@naver.com>
2024-05-02 22:39:15 +0900

    feat : Constants.kt, 카카오API 상수값 설정

---
+++ app/src/main/java/com/example/nbc_search/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.nbc_search
+
+class Constants {
+    companion object {
+        // kakaoAPI
+        const val BASE_URL = "https://dapi.kakao.com/v2/search/image"
+        const val AUTH_HEADER = "KakaoAK 2d6d59a869963226bc8390c125b99056"
+    }
+}
