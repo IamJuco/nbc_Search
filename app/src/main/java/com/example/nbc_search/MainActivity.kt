403ab3ee3ef77d40e7dfbef34546722cda6f0cac
Juseonghyun <wntjdgus99@naver.com>
2024-05-02 16:05:14 +0900

    feat : ViewPager2Adapter와 tabLayout MainActivity.kt에서 연결 완료

--- app/src/main/java/com/example/nbc_search/MainActivity.kt
+++ app/src/main/java/com/example/nbc_search/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.nbc_search
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.nbc_search.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var viewPager2Adapter: ViewPager2Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        setupViewPager2()
+
+    }
+
+    private fun setupViewPager2() {
+
+        viewPager2Adapter = ViewPager2Adapter(this)
+        viewPager2Adapter.addFragment(SearchFragment())
+        viewPager2Adapter.addFragment(StorageFragment())
+
+        binding.apply {
+            vpArea.adapter = viewPager2Adapter
+            TabLayoutMediator(tabMain, vpArea) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "이미지 검색"
+                    1 -> tab.text = "내 보관함"
+                }
+            }.attach()
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/nbc_search/ViewPager2Adapter.kt
@@ -0,0 +1,23 @@
+package com.example.nbc_search
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPager2Adapter(items: FragmentActivity) : FragmentStateAdapter(items) {
+
+    private var fragments: ArrayList<Fragment> = ArrayList()
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    fun addFragment(fragment: Fragment) {
+        fragments.add(fragment)
+        notifyItemInserted(fragments.size -1)
+    }
+}
