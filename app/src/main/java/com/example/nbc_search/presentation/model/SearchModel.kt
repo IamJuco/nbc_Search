707694e19bde887068d780334215b780f02654d4
Juseonghyun <wntjdgus99@naver.com>
2024-05-08 16:03:29 +0900

    chore : Parcelable 플러그인 제거

--- app/src/main/java/com/example/nbc_search/presentation/model/SearchModel.kt
+++ app/src/main/java/com/example/nbc_search/presentation/model/SearchModel.kt
@@ -1,13 +1,10 @@
 package com.example.nbc_search.presentation.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import java.util.Date
 
-@Parcelize
 data class SearchModel (
     val thumbnailUrl: String,
     val siteName: String,
     val dateTime: Date,
     var favorite: Boolean = false
-) : Parcelable
+)
